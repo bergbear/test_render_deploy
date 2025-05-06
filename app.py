2ae2f3866cd8a8e89e721a3e14199a4d350e6d37
bergbear <bergbear@gmail.com>
2025-05-06 14:06:26 -0700

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on Render!"
+    return "Hello from Flask on Render with a favicon!"
 
 
 if __name__ == "__main__":
